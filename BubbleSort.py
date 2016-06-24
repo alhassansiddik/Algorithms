18fea68454cd55d78276f483c90a5c1a45070144
alhassansiddik <alhassansiddik@yahoo.com>
2016-06-24 00:36:58 -0400

    Bubble Sort in Python and Java

---
+++ BubbleSort.py
@@ -0,0 +1,15 @@
+# takes an array as input and returns a sorted array using bubble sort algorithm
+class BubbleSort:
+    def sort(array):
+        for i in range(len(array) - 1, 0, -1):
+            for j in range(0, i):
+                if array[j] > array[j + 1]:
+                    temp = array[j]
+                    array[j] = array[j + 1]
+                    array[j + 1] = temp
+        return array
+
+    testArray = [3, 7, 5, 9, 2]
+    sortedArray = sort(testArray)
+    for i in testArray:
+        print(i)
